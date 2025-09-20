fd62b651ab7b2c301416fffd3c46571b6902d019
andyshera <andyshera123@gmail.com>
2025-09-20 14:41:54 +0700

    First files installed js from local desktop

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Hello, AI Short Video Maker!</h1>
+      <p>Selamat datang, project Next.js kamu sudah aktif 🎉</p>
+    </main>
+  );
+}
